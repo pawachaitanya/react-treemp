bb1ebbff186276c0a21c72ce6ad65d3a9fba2084
Chaitanya Pawa <chaitanya.pawa@mtxb2b.com>
2019-07-17 14:07:19 -0400

    hello

--- dist/react.d3.treemap.js
+++ dist/react.d3.treemap.js
@@ -3071,12 +3071,31 @@
                         y: o
                     }, u))
                 }, t.prototype._getLabelNewLine = function() {
+                    function nFormatter(num, digits) {
+                        var si = [
+                          { value: 1, symbol: "" },
+                          { value: 1E3, symbol: "k" },
+                          { value: 1E6, symbol: "M" },
+                          { value: 1E9, symbol: "G" },
+                          { value: 1E12, symbol: "T" },
+                          { value: 1E15, symbol: "P" },
+                          { value: 1E18, symbol: "E" }
+                        ];
+                        var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+                        var i;
+                        for (i = si.length - 1; i > 0; i--) {
+                          if (num >= si[i].value) {
+                            break;
+                          }
+                        }
+                        return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+                      }
                     
                     var e = this.props,
                         t = e.label,
                         n = e.textColor,
                         r = e.fontSize,
-                        c = e.valueWithFormat,
+                        c = String(nFormatter(parseInt(e.valueWithFormat.replace(',','')),1))
                         o = e.valueUnit,
                         i = e.hasChildren,
                         u = (e.nodeTotalNodes, e.globalTotalNodes, e.hideValue);
